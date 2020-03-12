2c70ff04c7cd214082c2be07d3da8c42436c7037
Andrey Kropotin <tork713@gmail.com>
2020-03-12 16:57:21 +0700

    Создал метод Hand и протестировал

--- 1_1.py
+++ 1_1.py
@@ -1,25 +1,27 @@
 import random
 
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')  # Масть
+# Задаем глобальные переменные:
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')  # Масть - Кортеж
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
-         'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+         'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')  # ранг карт - Кортеж
+values = {'Two': 2, 'Three': 3, 'Four': 4,
+          'Five': 5, 'Six': 6, 'Seven': 7,
+          'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10,
+          'Queen': 10, 'King': 10, 'Ace': 11}  # Стоимость карты в зависимости от её ранга
 
-values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
-          'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
-
-playing = True
+playing = True  # Переменная разрешающая или запрещающая игру
 
 
-class Card():
+class Card():  # класс репрезентующий одну карту по масти и рангу
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
 
-    def __str__(self):
+    def __str__(self):  # Специальный метод выводящий на экран
         return self.rank + " of " + self.suit
 
 
-class Deck():
+class Deck():  # Класс Колода 
     def __init__(self):
         self.deck = []  # start with an empty list
         for suit in suits:
@@ -40,9 +42,28 @@ class Deck():
         return single_card
 
 
+# Создаём класс Hand который будет отвечать за карты на руках у игрока
+class Hand():
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self, card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+
+    def adjust_for_ace(self):
+        pass
+
+
 test_deck = Deck()
 test_deck.shuffle()
-print(test_deck)
-print()
-print()
-print(test_deck.deal())
+
+# PLAYER
+test_player = Hand()
+
+pulled_card = test_deck.deal()
+print(pulled_card)
+test_player.add_card(pulled_card)
+print(test_player.value)
